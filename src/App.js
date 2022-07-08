0b49e1cb0a17b5d7f88fa5f8e0ef06b819e3a73f
Glebov Evgeny <egoji@mail.ru>
2022-07-08 10:11:59 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import {useEffect} from "react";
 import { Header } from "./components/header/header";
 import { Intro } from "./components/intro/intro";
 import { Footer } from "./components/footer/footer";
 
 
 function App() {
+    useEffect(
+        () => {
+            stickyHeader();
+            scrollToElement();
+        }, []
+    );
+
+    function scrollToElement() {
+        document.querySelectorAll('[data-scroll]').forEach(link => {
+            link.addEventListener('click', function(e) {
+                e.preventDefault();
+
+                let href = this.getAttribute('data-scroll');
+                const scrollTarget = document.getElementById(href),
+                    topOffset = document.querySelector('.header').offsetHeight,
+                    elementPosition = scrollTarget.getBoundingClientRect().top,
+                    offsetPosition = elementPosition - topOffset;
+
+                window.scrollBy({
+                    top: offsetPosition,
+                    behavior: 'smooth'
+                });
+            });
+        });
+    }
+
+    function stickyHeader() {
+        const header = document.querySelector('.header');
+        let scrollPrev = 0;
+
+        document.addEventListener('scroll', function(e) {
+            let scrolled = window.scrollY;
+            if ( scrolled > 50 && scrolled > scrollPrev ) {
+                header.classList.add('out');
+            } else {
+                header.classList.remove('out');
+            }
+            scrollPrev = scrolled;
+        })
+    }
+
     return (
         <>
             <Header/>
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,63 +1,35 @@
 import React from "react";
-import {useEffect} from "react";
-
 import {
     Link
 } from "react-router-dom";
+import tiger from "../../assets/img/common/tiger.png";
+
 
 function Header() {
-    useEffect(
-        () => {
-            stickyHeader();
-            scrollToElement();
-        }, []
-    );
-
-    function scrollToElement() {
-        document.querySelectorAll('[data-scroll]').forEach(link => {
-            link.addEventListener('click', function(e) {
-                e.preventDefault();
-
-                let href = this.getAttribute('data-scroll');
-                const scrollTarget = document.getElementById(href);
-                const topOffset = document.querySelector('.header').offsetHeight;
-                // const topOffset = 0; // если не нужен отступ сверху
-                const elementPosition = scrollTarget.getBoundingClientRect().top;
-                const offsetPosition = elementPosition - topOffset;
-
-                window.scrollBy({
-                    top: offsetPosition,
-                    behavior: 'smooth'
-                });
-            });
-        });
-    }
-
-    function stickyHeader() {
-        const header = document.querySelector('.header');
-        let scrollPrev = 0;
-
-        document.addEventListener('scroll', function(e) {
-            let scrolled = window.scrollY;
-            if ( scrolled > 100 && scrolled > scrollPrev ) {
-                header.classList.add('out');
-            } else {
-                header.classList.remove('out');
-            }
-            scrollPrev = scrolled;
-
-        })
+    function menuOnClick() {
+        document.querySelector(".header__burger").classList.toggle("open");
+        document.querySelector(".header__nav").classList.toggle("open");
+        document.querySelector(".header__bg").classList.toggle("open");
     }
 
     return (
         <header className="header">
-            <div className="container">
-                <div className="header__logo"></div>
+            <div className="container header__container">
+                <div className="header__logo">
+                    <p>Типа<br/>Лого</p>
+                    {/*<img src={tiger} className="header__logo-pic" alt="tiger icon" />*/}
+                </div>
                 <nav className="header__nav">
                     <Link to="/" className="header__nav-link">Home</Link>
                     <Link to="/test" className="header__nav-link">Test</Link>
                 </nav>
-                <button className="header__menu" data-scroll="target">xxx</button>
+                <button className="header__burger" onClick={()=>menuOnClick()}>
+                    <div className="header__burger-bar _01"></div>
+                    <div className="header__burger-bar _02"></div>
+                    <div className="header__burger-bar _03"></div>
+                </button>
+                <div className="header__bg"></div>
+                {/*<button className="header__menu" data-scroll="target">xxx</button>*/}
             </div>
         </header>
     );
